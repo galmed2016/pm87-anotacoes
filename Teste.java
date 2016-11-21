69d5795c5bff578f29be19fa25f8e4fa1481124b
Erico Gomes <erico_gomes@hotmail.com>
2016-11-21 17:52:37 -0200

    SEGUNDA ALTERAÇAO PARA ESTUDOS

--- Teste.java
+++ Teste.java
@@ -1,5 +1,11 @@
 public class TesteGit {
   public static void main( String[] args ){
     System.out.println("Exemplo de teste de uso do git" );
+    System.out.println("Exemplo de teste de uso do git" );
    }
+
+  private void teste(){
+   System.out.println("OUTRO Exemplo de teste de uso do git" );
+ 
+}
 }
