917344d27897f876b8adb38c31f882a3bdc2cbe2
Erico Gomes <erico_gomes@hotmail.com>
2016-11-22 14:56:10 -0200

    MAIS UMA MENSAGEM dia 22/11/2016

--- Teste.java
+++ Teste.java
@@ -1,11 +1,15 @@
 public class TesteGit {
   public static void main( String[] args ){
     System.out.println("Exemplo de teste de uso do git" );
-    System.out.println("Exemplo de teste de uso do git" );
+    System.out.println("TRABALHANDO COM BRANCHES" );
    }
 
   private void teste(){
    System.out.println("OUTRO Exemplo de teste de uso do git" );
  
+}
+  private int OutroTeste(){
+
+   System.out.println("OOUTRO TESTE DE TESTE" );
 }
 }
